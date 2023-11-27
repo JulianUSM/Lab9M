eca6027c2d97c9c0685187ad30435a0112cf5f6a
PerisicJere <117933956+PerisicJere@users.noreply.github.com>
2023-11-27 14:53:17 -0500

    Sort method

--- Lab9/HeapSort.java
+++ Lab9/HeapSort.java
@@ -5,20 +5,24 @@ package Lab9;
  * @version November 27, 2023
  */
 public class HeapSort {
-    
+    /**
+     * constructor
+     */
+    public HeapSort(){
+
+    }
     /**
      * This method takes in an array on integers and sort them using heap-sort
      * @param input array of integer items
      * @return sorted array
      */
 
-
     public static int[] sort(int[]input)
     {
         int n = input.length;
         MaxHeap maxHeap = new MaxHeap(input);
 
-        for (int i = n - 1; i >= 0; i--) {
+        for (int i = n - 1; i >= 0 ; i--) {
             int temp = maxHeap.delete();
             input[i] = temp;
         }
