8aaa0c71d34a5d15501bc13cb3b8c004c81552eb
jbredwards <143640517+JulianUSM@users.noreply.github.com>
2023-11-27 19:26:06 -0500

    Create RunningExample.java

---
+++ Lab9/RunningExample.java
@@ -0,0 +1,16 @@
+package Lab9;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @since 27 November 2023
+ * @author Julian Edwards
+ *
+ */
+public final class RunningExample
+{
+    public static void main(final String[] args) {
+        System.out.println(Arrays.toString(HeapSort.sort(new int[] {4, 2, 5, 1, 3, 6})));
+    }
+}
